cb424bca78039990cc474eab780cfdd4d9770c25
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-09 03:15:26 +0000

    Add files via upload

--- gellany_freeze_bash.sh
+++ gellany_freeze_bash.sh
@@ -7,5 +7,5 @@ pip3 freeze --all > requirements2.txt
 #head $1|while read line ; do grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/^/\^/';done > test.txt
 #cat test.txt|while read line;do grep -iE $line requirements2.txt;done > requirements.txt
 
-head $1|while read line ; do grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/^/\^/'|while read lineB;do grep -iE $lineB requirements2.txt;done;done > requirements.txt 
+sort $1|while read line ; do grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/^/\^/'|while read lineB;do grep -iE $lineB requirements2.txt;done;done > requirements.txt 
 
