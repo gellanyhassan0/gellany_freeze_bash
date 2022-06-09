f739e23b8d1ef17cf91f3a7f30f8de500c8a1f0c
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-09 01:23:46 +0000

    Add files via upload

--- gellany_freeze_bash.sh
+++ gellany_freeze_bash.sh
@@ -4,6 +4,6 @@ pip install freez
 
 pip3 freeze --all > requirements2.txt
 
-head $1|while read line ; do grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g';done > test.txt
+head $1|while read line ; do grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/$/=/';done > test.txt
 
 grep -if test.txt requirements2.txt > requirements.txt
