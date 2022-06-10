b844319ac89d799de6a53d86b6133df69bd88152
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-10 23:45:23 +0000

    Update gellany_freeze_bash.sh

--- gellany_freeze_bash.sh
+++ gellany_freeze_bash.sh
@@ -2,7 +2,7 @@
 
 pip install freez
 
-pip3 freeze --all >> requirements2.txt
+pip3 freeze --all > requirements2.txt
 
 #head $1|while read line ; do grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/^/\^/';done > test.txt
 #cat test.txt|while read line;do grep -iE $line requirements2.txt;done > requirements.txt
