5e63a1c34d7b369d631969a85c217604d6f4fa49
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-23 07:36:14 +0000

    Add files via upload

--- gellany_freeze_bash_folder.sh
+++ gellany_freeze_bash_folder.sh
@@ -4,4 +4,4 @@ pip install freez
 
 pip3 freeze --all > requirements2.txt
 
-find $1 -type f -name "*.py"|while read line ; do cat $line|grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/^/\^/'|while read lineB;do grep -siE --no-filename  $lineB requirements2.txt 2>/dev/null;done;done|tee requirements.txt
+find $1 -type f -name "*.py"|while read line ; do cat $line|grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/^/\^/'|while read lineB;do grep -siE --no-filename  $lineB requirements2.txt 2>/dev/null;done;done|sort -uf|tee requirements.txt
