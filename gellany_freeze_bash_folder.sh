135741f2de166ab0856f04f1f18f001663e30027
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-24 16:35:54 +0000

    Update gellany_freeze_bash_folder.sh

--- gellany_freeze_bash_folder.sh
+++ gellany_freeze_bash_folder.sh
@@ -4,4 +4,4 @@ pip install freez
 
 pip3 freeze --all > requirements2.txt
 
-find $1 -type f -name "*.py"|while read line ; do cat $line|grep -ioE "import [a-zA-Z0-9]+"|sed 's/import //g'|sed -e 's/^/\^/'|while read lineB;do grep -siE --no-filename  $lineB requirements2.txt 2>/dev/null;done;done|sort -uf|tee requirements.txt
+find $1 -type f -name "*.py"|while read line ; do cat $line|grep -ioE "import [a-zA-Z0-9]+|from [a-zA-Z0-9]+"|sed 's/import //g; s/from //g'|sed -e 's/^/\^/'|sed -e 's/^/\^/'|while read lineB;do grep -siE --no-filename  $lineB requirements2.txt 2>/dev/null;done;done|sort -uf|tee requirements.txt
